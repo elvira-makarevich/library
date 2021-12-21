844a0ece60f725515036614192f0e27beec13b95
Elvira Makarevich <makarevich-elvira@mail.ru>
2021-12-21 03:04:57 +0300

    add encoding filter

---
+++ src/main/java/com/ita/u1/library/controller/filter/EncodingFilter.java
@@ -0,0 +1,31 @@
+package com.ita.u1.library.controller.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class EncodingFilter implements Filter {
+
+    public static final String CONFIG_INIT_PARAM = "characterEncoding";
+    private String encoding = "utf-8";
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        encoding = filterConfig.getInitParameter(CONFIG_INIT_PARAM);
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        req.setCharacterEncoding(encoding);
+        res.setCharacterEncoding(encoding);
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
